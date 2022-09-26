6967ec934a265d783e36157ab72a5f01ba5de192
mucahitarslaan <mucahitarslaan@gmail.com>
2022-09-26 16:30:25 +0300

    GenericAppIntelliJTemplate

--- GenericAppIntellij/src/main/java/org/arslan/application/App.java
+++ GenericAppIntellij/src/main/java/org/arslan/application/App.java
@@ -1,4 +1,4 @@
-package org.arslan.application.createobjectauto;
+package org.arslan.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
