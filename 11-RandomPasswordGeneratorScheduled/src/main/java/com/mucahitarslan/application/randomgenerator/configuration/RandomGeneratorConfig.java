3d7e10cac0faa083f78204bef45dfc2dcb669eb3
mucahitarslaan <mucahitarslaan@gmail.com>
2022-11-21 19:24:38 +0300

    update

---
+++ 11-RandomPasswordGeneratorScheduled/src/main/java/com/mucahitarslan/application/randomgenerator/configuration/RandomGeneratorConfig.java
@@ -0,0 +1,18 @@
+package com.mucahitarslan.application.randomgenerator.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+import java.util.Random;
+
+@Configuration
+public class RandomGeneratorConfig {
+
+    @Bean
+    @Scope("prototype")
+    public Random getRandom()
+    {
+        return new Random();
+    }
+}
---
+++ 11-RandomPasswordGeneratorScheduled/src/main/java/com/mucahitarslan/application/randomgenerator/runner/RandomPasswordGeneratorSecond.java
@@ -0,0 +1,54 @@
+package com.mucahitarslan.application.randomgenerator.runner;
+
+import org.csystem.util.scheduler.Scheduler;
+import org.csystem.util.string.StringUtil;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedWriter;
+import java.util.Random;
+
+@Component
+public class RandomPasswordGeneratorSecond implements ApplicationRunner {
+
+    private BufferedWriter bufferedWriter;
+    private Random random;
+    private Scheduler scheduler;
+
+    @Value("${count}")
+    private int count;
+
+    @Value("${initial}")
+    private int initial;
+
+    private void schedulerCallback()
+    {
+        try {
+            if (count == initial)
+                scheduler.cancel();
+            ++initial;
+
+            bufferedWriter.write(StringUtil.getRandomTextEN(random,10) + "\r\n");
+            bufferedWriter.flush();
+
+        }catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public RandomPasswordGeneratorSecond(@Qualifier("bufferedWriter.second") BufferedWriter bufferedWriter, Random random, Scheduler scheduler) {
+        this.bufferedWriter = bufferedWriter;
+        this.random = random;
+        this.scheduler = scheduler;
+    }
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        scheduler.schedule(this::schedulerCallback);
+    }
+}
