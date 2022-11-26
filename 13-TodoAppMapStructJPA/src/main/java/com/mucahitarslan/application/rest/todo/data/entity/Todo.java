89c48e03085390b429f8b8a0db2b37bd5cdfb0c8
mucahitarslaan <mucahitarslaan@gmail.com>
2022-11-26 18:11:48 +0300

    update

---
+++ 13-TodoAppMapStructJPA/src/main/java/com/mucahitarslan/application/rest/todo/data/entity/Todo.java
@@ -0,0 +1,30 @@
+package com.mucahitarslan.application.rest.todo.data.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "todoappdb")
+public class Todo{//POJO (Plain Old Java Object)
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "todo_id")
+    public long id;
+
+    @Column(name = "title", length = 128, nullable = false)
+    public String title;
+
+    @Column(name = "text", length = 512)
+    public String text;
+
+    @Column(name = "instert_date_time", nullable = false)
+    public LocalDateTime insertDateTime = LocalDateTime.now();
+
+    @Column(name = "last_update", nullable = false)
+    public LocalDateTime lastUpdate = insertDateTime;
+
+    @Column(name = "completed", nullable = false)
+    public boolean completed;
+
+}
