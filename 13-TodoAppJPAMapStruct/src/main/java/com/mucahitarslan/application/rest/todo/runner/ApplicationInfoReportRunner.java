20fcbb695dd12863af8ccf5de92c3cb168f12019
mucahitarslaan <mucahitarslaan@gmail.com>
2022-11-27 03:23:53 +0300

    update

--- 13-TodoAppJPAMapStruct/src/main/java/com/mucahitarslan/application/rest/todo/runner/ApplicationInfoReportRunner.java
+++ 13-TodoAppJPAMapStruct/src/main/java/com/mucahitarslan/application/rest/todo/runner/ApplicationInfoReportRunner.java
@@ -1,10 +1,14 @@
 package com.mucahitarslan.application.rest.todo.runner;
 
+import com.mucahitarslan.application.rest.todo.data.repository.ITodoRepository;
 import org.csystem.util.Console;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
+
+import javax.persistence.PersistenceContext;
+import java.util.stream.Stream;
 
 @Component
 public class ApplicationInfoReportRunner implements ApplicationRunner {
@@ -16,6 +20,23 @@ public class ApplicationInfoReportRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-     Console.writeLine("Bean definition count:%d",applicationContext.getBeanDefinitionCount());
+
+        Console.writeLine("---------------");
+        Console.writeLine("-----Todos-----");
+        ITodoRepository todoResository = applicationContext.getBean(ITodoRepository.class);
+
+        todoResository.findAll().forEach(t -> Console.writeLine(t.title));
+
+        Console.writeLine("-----Application Info-----");
+        Console.writeLine("Application Name :%s", applicationContext.getApplicationName());
+        Console.writeLine("Application ID :%s", applicationContext.getId());
+
+        Console.writeLine("-----Bean/Component Info-----");
+        Console.writeLine("Bean definition count :%d", applicationContext.getBeanDefinitionCount());
+
+        Console.writeLine("-----Beans-----");
+        Stream.of(applicationContext.getBeanDefinitionNames()).forEach(Console::writeLine);
+        Console.writeLine("----------------");
+
     }
 }
